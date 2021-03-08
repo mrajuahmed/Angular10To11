a84dc1f41f3545be0ae871d734b247e7fa68ec42
raju_home <rajuahmed.329@gmail.com>
2021-03-08 21:03:25 +0600

    Angular 10 - 11 Hindi tutorial #8 call function | call function: Done.

--- blog-angular/src/app/app.component.ts
+++ blog-angular/src/app/app.component.ts
@@ -8,14 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Blog';
   name="Raju Ahmed.";
+
   getFullBlogTitile(){
     return "This "+ this.title +" is developed by "+this.name;
   }
+
   blogerObj={
     Id:1,
     Name:"Marginal Raju",
     Designation:"Software Engineer"
   }
 
-  bloggerArray=['Azad','Sayed','Shohag','Anny']
+  bloggerArray=['Azad','Sayed','Shohag','Anny'];
+
+  firstButtonFunction(){
+    var message="Your First Function Was Successfully Binded With This Button ";
+    console.warn(message);
+    alert(message);
+  }
 }
