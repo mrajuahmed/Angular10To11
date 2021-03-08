28c1e203daa84070c3b64a3c82f8c4a8a0f87451
raju_home <rajuahmed.329@gmail.com>
2021-03-08 23:13:49 +0600

    Angular 10 Hindi tutorial #9 important event in angular: done

--- blog-angular/src/app/app.component.ts
+++ blog-angular/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
     Id:1,
     Name:"Marginal Raju",
     Designation:"Software Engineer"
-  }
+  };
 
   bloggerArray=['Azad','Sayed','Shohag','Anny'];
 
@@ -25,5 +25,46 @@ export class AppComponent {
     var message="Your First Function Was Successfully Binded With This Button ";
     console.warn(message);
     alert(message);
+  };
+  emptyValue="__________";
+  myClickEventValue=this.emptyValue;
+  myClickEvent(evt){
+    this.myClickEventValue=evt;
+    this.emptyValue=evt;
+  }
+
+  myMouseOverEventValue=this.emptyValue;
+  myMouseOverEvent(evt){
+    this.myMouseOverEventValue=evt;
+  }
+
+  myMouseEnterEventValue=this.emptyValue;
+  myMouseEnterEvent(evt){
+    this.myMouseEnterEventValue=evt;
+  }
+
+  myMouseLeaveEventValue=this.emptyValue;
+  myMouseLeaveEvent(evt){
+    this.myMouseLeaveEventValue=evt;
+  }
+
+  keyupEvntValue=this.emptyValue;
+  myKeyUpEvent(vle){
+    this.keyupEvntValue=vle;
+  }
+
+  myKeyUpEnterEventValue=this.emptyValue;
+  myKeyUpEnterEvent(evt){
+    this.myKeyUpEnterEventValue=evt;
+  }
+
+  myKeyUpSpaceEventValue=this.emptyValue;
+  myKeyUpSpaceEvent(evt){
+    this.myKeyUpSpaceEventValue=evt;
+  }
+
+  keyDownEvntValue=this.emptyValue;
+  myKeyDownEvent(evt){
+    this.keyDownEvntValue=evt;
   }
 }
