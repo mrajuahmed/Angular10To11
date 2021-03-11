3da475b68ae7365393268b6081aa8b9a4073272f
Raju Ahmed <rajuahmed.329@gmail.com>
2021-03-11 13:13:14 +0600

    Angular 10 tutorial #19 Add Material UI:Done

--- blog-angular/src/app/app.module.ts
+++ blog-angular/src/app/app.module.ts
@@ -7,19 +7,28 @@ import { AppComponent } from './app.component';
 import { FormsModule }from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatButtonModule} from '@angular/material/button'
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
